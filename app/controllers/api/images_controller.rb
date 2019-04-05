7f7c8df7cf3080752daaefe20a4fc58afa34fbd1
Rusell <russell.brady@gmx.com>
2019-04-05 15:41:22 -0600

    initial

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,16 +1,41 @@
 class Api::ImagesController < ApplicationController
   def index
+    render json: Image.all
   end
 
   def show
+    render json: @image
   end
 
   def create
+    image = Image.new(image_params)
+
+    if image.save
+      render json: image
+    else
+      render json: image.errors, status: 422 
+    end
   end
 
   def update
+    if @image.update(image_params)
+      render json: @image
+    else
+      render json: @images.errors, status: 422
+    end
   end
 
   def destroy
+    @image.destroy
   end
+
+  private
+    def set_image
+      @image = Image.find(params[:id])
+    end
+
+    def _params
+      params.require(:image).permit(:name, :description, :price, :department)
+    end
+
 end
