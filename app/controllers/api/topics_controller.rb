424facb66ac75949a664c3a6f31c9a07d66244e0
Sione Tenifa <johntenifa@yahoo.com>
2019-04-05 15:30:18 -0600

    topic and event controller built out

--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -1,16 +1,42 @@
 class Api::TopicsController < ApplicationController
+  before_action :set_topics, only: [:show, :update, :destroy]
+
   def index
+    render json: Topic.all
   end
 
   def show
+    render json: @topic
   end
 
   def create
+    topic = Topic.new(topic_params)
+
+    if topic.save
+      render json: topic
+    else
+      render json: topic.errors, status: 422
+    end
   end
 
   def update
+    if @topic.update(topic_params)
+      render json: @topic
+    else
+      render json: @topic.errors, status: 422
+    end
   end
 
   def destroy
+    @topic.destroy
   end
+
+  private
+    def set_topics
+      @topic = Topic.find(params[:id])
+    end
+
+    def topic_params
+      params.require(:topic).permit(:title, :topic_date, :topic_image,)
+    end
 end
