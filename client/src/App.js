2b6b834b0d6bc0c279d67d9420bdcd31ee2fbdc2
Mike Coker <mbcoker100@gmail.com>
2019-04-08 14:42:09 -0600

    auth update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import React, { Fragment, } from 'react';
-import Home from './components/Home';
-import NoMatch from './components/NoMatch';
-import Login from './components/Login';
-import Register from './components/Register';
 import { Switch, Route, } from 'react-router-dom';
 import { Container, } from "semantic-ui-react";
+import Home from './components/Home';
+import NoMatch from './components/NoMatch';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import FetchUser from './components/FetchUser'
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => (
-  <Fragment>
+  <div>
+  <Navbar />
+  <FetchUser>
     <Container>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <ProtectedRoute exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
-  </Fragment>
+  </FetchUser>
+</div>
 )
 
 export default App;
-  
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, } from 'react-router-dom';
 import { AuthProvider, } from "./providers/AuthProvider";
 import 'semantic-ui-css/semantic.min.css';
+import { initMiddleware, } from 'devise-axios';
+
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
@@ -14,5 +16,3 @@ ReactDOM.render(
   </AuthProvider>,
   document.getElementById('root')
 );
-
-registerServiceWorker();
