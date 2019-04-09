4b48d6d730d1ff50e8589a14870c5e177a799194
Brandon Parker <newboots@gmail.com>
2019-04-09 11:17:30 -0600

    vidcomponentsandseed

---
+++ client/src/components/Video.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+class Video extends React.Component {
+  state = { videos: [] }
+  
+  componentDidMount() {
+    axios.get(`/api/videos`)
+      .then(res =>
+        this.setState({ videos: res.data }))
+  }
+
+  render() {
+    return (
+      <>
+        <div style={{display: "flex", flexWrap: "wrap"}}>
+          {this.state.videos.map(v=> 
+            <div key={this.state.videos.id} style={{margin: "20px", zIndex: "2", border: "solid black 1px", textAlign: "Center"}}> 
+              <Link to={`/video/${v.id}`}>    
+                <div style={{}}>
+                  <iframe src={v.video_url} controls={0} style={{zIndex: "1"}} >
+                  </iframe>
+                </div>
+                <div style={{display: "flex", width: "100%"}}>
+                <div style={{fontSize: "2em", fontWeight: "bold", padding: "10px", color: "black"}}>
+                {v.title} 
+                </div>
+                <div style={{fontSize: "1em", color: "black", }}></div>
+                </div>
+              </Link>
+              </div>)}
+        </div>
+    
+      </>
+    )
+  }
+
+}
+
+
+
+
+export default Video;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-
-Video.create(title: "A sweet video 1", description: "https://www.youtube.com/embed/FcnhekTNHPg")
-Video.create(title: "A sweet video 2", description: "https://www.youtube.com/embed/AxM9FYSs8V4")
-Video.create(title: "A sweet video 3", description: "https://www.youtube.com/embed/5iV_hB08Uns")
-Video.create(title: "A sweet video 4", description: "https://www.youtube.com/embed/t0_JMBPOdLw")
-Video.create(title: "A sweet video 5", description: "https://www.youtube.com/embed/xIqza5kdmdQ")
+Video.create(title: "A sweet video 1", video_url: "https://www.youtube.com/embed/FcnhekTNHPg", description: "Sweetvideo")
+Video.create(title: "A sweet video 2", video_url: "https://www.youtube.com/embed/AxM9FYSs8V4", description: "Sweetvideo")
+Video.create(title: "A sweet video 3", video_url: "https://www.youtube.com/embed/5iV_hB08Uns", description: "Sweetvideo")
+Video.create(title: "A sweet video 4", video_url: "https://www.youtube.com/embed/t0_JMBPOdLw", description: "Sweetvideo")
+Video.create(title: "A sweet video 5", video_url: "https://www.youtube.com/embed/xIqza5kdmdQ", description: "Sweetvideo")
 
 puts "seeded"
