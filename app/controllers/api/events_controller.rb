1f013c01b42dd70da197dac15a58b28501bfea22
Rusell <russell.brady@gmx.com>
2019-04-14 16:46:07 -0600

    add events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,7 +2,8 @@ class Api::EventsController < ApplicationController
   before_action :set_event, only: [:show, :update, :destroy]
 
   def index
-    render json: Event.all
+    render json: Event.all.map{|event|{title: event.title, start_time:event.start_time.strftime("%Y-%m-%d %I:%M%p"),
+    end_time:event.end_time.strftime("%Y-%m-%d %I:%M%p")}}
   end
 
   def show
@@ -11,6 +12,10 @@ class Api::EventsController < ApplicationController
 
   def create
     event = Event.new(event_params)
+    start_time = DateTime.now + 1
+    endtime = DateTime.now + 2
+    event.start_time = start_time
+    event.end_time = endtime
 
     if event.save
       render json: event
